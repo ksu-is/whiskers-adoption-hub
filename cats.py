5ad838edb319d8e985d7071d2d1e00a2e14b2e8b
jackie mendoza <jmendo38@students.kennesaw.edu>
2026-05-05 12:40:04 -0400

    More cats
    Corrected syntax errors and added new cat entries.

--- cats.py
+++ cats.py
@@ -1,7 +1,12 @@
 cats = [
-    {"name": "Milo", "age": 2, "breed": "Tabby", "status": "Available"}
-    {"name": "Kyo" , "age": 4, "breed": "American shorthair" "status":"Adopted"}
-    {"name": "Leo" , "age": 7  "breed":  "Maincoon" "status": "Available"}
-    {"name": "Bambi", "age":1  "breed": "Ragdoll" "status": "Pending"}
-    {"name": "Theo", "age": 3  "breed": "Scottish fold" "status": "Available"}
-    ]
+    {"name": "Milo", "age": 2, "breed": "Tabby", "status": "Available"},
+    {"name": "Kyo", "age": 4, "breed": "American shorthair", "status": "Adopted"},
+    {"name": "Leo", "age": 7, "breed": "Maine Coon", "status": "Available"},
+    {"name": "Bambi", "age": 1, "breed": "Ragdoll", "status": "Pending"},
+    {"name": "Theo", "age": 3, "breed": "Scottish Fold", "status": "Available"}
+    {"name": "Ivy", "age": 3, "breed": "Ragdoll", "status": "Adopted"}
+    {"name": "Tiger", "age": 2, "breed": "Main coon", "status": "Pending"}
+    {"name": "Cheese", "age": 5, "breed": "Scottish Fold", "status": "Adopted"}
+    {"name": "Miles", "age": 10, "breed": "American shorthair", "status": "Available"}
+]
+    
